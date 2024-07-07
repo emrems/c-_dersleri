51ff33501e83b46217bedeb1663e0499527daa1f
emre almamis <ealmamis56@gmail.com>
2024-07-07 14:54:55 +0300

    classlar konusu bitti

---
+++ class örnek 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_örnek_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ögrenci ögrenci1 = new Ögrenci(3223,"emre","almamıs",65,87,89,"SUBU");
+            bool kontrol = true;
+            while (kontrol)
+            {
+                int gelenSecim = secimYap();
+                switch (gelenSecim)
+                {
+                    case 1:
+                        ögrenci1.ogrenciBilgileriGetir();
+                        break;
+                    case 2:
+                        double not = ögrenci1.ortalamaGetir();
+                        Console.WriteLine(not);
+
+                        break;
+                    case 3:
+                        ögrenci1.okulGetir();
+                        break;
+                    case 4:
+                        Console.WriteLine("çıkış yapılıyor");
+                        break;
+                    default:
+                        Console.WriteLine("lütfen geçerli bir seçim yapınız");
+                        break;
+
+                }
+            }
+           
+            Console.ReadLine();
+
+        }
+
+        static int secimYap()
+        {
+            Console.WriteLine("1- öğrenci bilgileri göster");
+            Console.WriteLine("2-öğrenci ortalaması göster");
+            Console.WriteLine("3-öğrenci okul öğren");
+            Console.WriteLine("4-çıkış yap");
+
+            Console.WriteLine("lütfen bir seçim yapınız");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            return secim;
+
+        }
+    }
+}
---
+++ class örnek 1/Ögrenci.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_örnek_1
+{
+    public class Ögrenci
+    {
+        private int ogrenciNo;
+        private string isim;
+        private string soyad;
+        private int vize1;
+        private int vize2;
+        private string okulİsmi;
+        private int final;
+
+        public Ögrenci(int numara, string ad, string soyisim, int sinav1, int sinav2,int final1, string okulAdi)
+        {
+            ogrenciNo= numara;
+            isim= ad;
+            soyad= soyisim;
+            vize1= sinav1;
+            vize2= sinav2;
+            okulİsmi= okulAdi;
+            final= final1;
+
+        }
+
+        public void ogrenciBilgileriGetir()
+        {
+            Console.WriteLine("bilgileri istenen öğrencinin adı:" + isim + "dir" + "soyadı" +  soyad + "öğrenci numarası:" + ogrenciNo);
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("okul ismi" + okulİsmi);
+        }
+
+        public double ortalamaGetir()
+        {
+
+            double ortalama = (vize1 * 0.2) + (vize2 * 0.2) + (final * 0.6);
+            return ortalama;
+        }
+
+    }
+}
